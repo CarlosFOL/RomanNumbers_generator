7ec54f6690c388a8e3cb6c24be343832f191c718
Carlos Francisco Orejuela Lluncor <carlos_francisco_@hotmail.com>
2022-07-05 21:50:48 -0500

    Added a function that change int num -> roman num

--- roman_numbers.py
+++ roman_numbers.py
@@ -1,9 +1,31 @@
-def roman_number(limit_number):
-    int_number = 1
+SOME_NUMBERS = {
+        1: "I",
+        5: "V",
+        10: "X",
+        50: "L",
+        100: "C",
+        500: "D",
+        1000: "M"
+    }
+
+def transformation(num, x):
+    if 0<=num<4:
+        return SOME_NUMBERS[1*x]*(num)
+    elif 5<=num<9:
+        return SOME_NUMBERS[5*x]+(SOME_NUMBERS[1*x]*(num - 5))
+    elif num == 4:
+        return SOME_NUMBERS[1*x]+SOME_NUMBERS[5*x]
+    elif num == 9:
+        return SOME_NUMBERS[1*x]+SOME_NUMBERS[10*x]
+
+def roman_number(limit_num):
+    int_num = 1
     counter = 1
-
-
-
+    while True:
+        if counter <= limit_num:
+            if len(str(int_num)) == 1:
+                unit = transformation(int_num, 1)
+        else:break
 
 def run():
     limit_num = 1000
